784e87248658ba5a926a67f2bfe4687acb2a16dc
Alejandro Ruiz <angel_alexito123@hotmail.com>
2020-03-03 18:38:23 -0600

    pequeño socket

---
+++ server/index.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+var server = require("http").Server(app);
+var io = require('socket.io')(server);
+
+app.use(express.static('client'));
+
+app.get('/hola-mundo', function(req,res){
+    res.status(200).send('hola mundo desde una ruta');
+});
+
+var messages = [{
+    id: 1,
+    text: 'Bienvenido al chat privado de node Js y Socket.io',
+    nickname: 'bot - angel2101'
+}];
+
+io.on('connection', function(socket){
+    console.log('el cliente con IP '+ socket.handshake.address + ' se ha conectado...');
+
+    socket.emit('messages',  messages);
+
+    socket.on('add-message', function(data){
+        messages.push(data);
+
+        io.sockets.emit('messages',  messages);
+    });
+});
+
+server.listen(6677, function(){
+    console.log('el servidor está funcionando http://localhost:6677');
+    
+})
